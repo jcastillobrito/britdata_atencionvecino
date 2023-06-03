e1467412f44c0c2fb5c3e80f9307c4e09e7cdb9b
Juan Carlos <jccastillo.brito@gmail.com>
2023-06-02 22:31:45 -0400

    proyecto laravel 9 base

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,63 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="nk-wrap nk-wrap-nosidebar">
+    <!-- content @s -->
+    <div class="nk-content ">
+        <div class="nk-block nk-block-middle nk-auth-body  wide-xs">
+            <div class="brand-logo pb-4 text-center">
+                <a href="html/index.html" class="logo-link">
+                    <img class="logo-light logo-img logo-img-lg" src="./images/logo.png" srcset="./images/logo2x.png 2x" alt="logo">
+                    <img class="logo-dark logo-img logo-img-lg" src="./images/logo-dark.png" srcset="./images/logo-dark2x.png 2x" alt="logo-dark">
+                </a>
+            </div>
+            <div class="card card-bordered">
+                <div class="card-inner card-inner-lg">
+                    <div class="nk-block-head">
+                        <div class="nk-block-head-content">
+                            <h4 class="nk-block-title">Sign-In</h4>
+                            <div class="nk-block-des">
+                                <p>Access the Dashlite panel using your email and passcode.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+                        <div class="form-group">
+                            <div class="form-label-group">
+                                <label class="form-label" for="email">Email or Username</label>
+                            </div>
+                            <div class="form-control-wrap">
+                                <input type="text" class="form-control form-control-lg @error('email') is-invalid @enderror" id="email" name="email" value="{{ old('email') }}" placeholder="Enter your email address or username" required autocomplete="email" autofocus>
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="form-label-group">
+                                <label class="form-label" for="password">Passcode</label>
+                                <a class="link link-primary link-sm" href="{{ route('password.request') }}">Forgot Code?</a>
+                            </div>
+                            <div class="form-control-wrap">
+                                <a href="#" class="form-icon form-icon-right passcode-switch lg" data-target="password">
+                                    <em class="passcode-icon icon-show icon ni ni-eye"></em>
+                                    <em class="passcode-icon icon-hide icon ni ni-eye-off"></em>
+                                </a>
+                                <input type="password" class="form-control form-control-lg @error('password') is-invalid @enderror" id="password" name="password" placeholder="Enter your passcode" required autocomplete="current-password">
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-lg btn-primary btn-block">Sign in</button>
+                        </div>
+                    </form>
+                    <div class="form-note-s2 text-center pt-4"> New on our platform? <a href="{{ route('register') }}">Create an account</a>
+                    </div>
+                    <div
